733e40caa665eb7f08a7e510533fa3326c676245
EJCLO <102440468+EJCLO@users.noreply.github.com>
2022-06-26 20:13:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerText="if you deposit "+principal+"\nat an interest rate of "+rate+"\nYou will receive an amount of "+interest+"\nin the year of "+year;
+}
+        
+function updateRate(){
+    var reteval= document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=reteval;
 }
         
